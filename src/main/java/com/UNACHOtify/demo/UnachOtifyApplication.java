142d0e6fb5db798eada6f66b2944f3f1c79ca702
Carlos Reyes <carlosmxm055@gmail.com>
2024-05-13 22:47:11 -0600

    Project base

---
+++ src/main/java/com/UNACHOtify/demo/UnachOtifyApplication.java
@@ -0,0 +1,13 @@
+package com.UNACHOtify.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnachOtifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnachOtifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UNACHOtify/demo/UnachOtifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UNACHOtify.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnachOtifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
